d06691e7ff104e9e870cd56bbccdd5ec16f09b14
Olga Linikova <starshayayord@gmail.com>
2016-02-29 17:09:27 +0500

    Update autoreply.pl

--- autoreply.pl
+++ autoreply.pl
@@ -4,9 +4,9 @@ use strict;
 use MIME::Base64;
 
 my ($to, $from) = @ARGV;
-$from =~ s/autoreply.e-kontur.ru/e-kontur.ru/;
+$from =~ s/autoreply.domain.ru/domain.ru/;
 open MAIL, "| /usr/sbin/sendmail -t -oi";
-print MAIL "To: $to\nFrom: noreply\@e-kontur.ru\nSubject: =?utf-8?B?0JLQsNGI0LUg0L/QuNGB0YzQvNC+INC90LUg0YPQtNCw0LvQvtGB0Ywg0LQ=?= =?utf-8?B?0L7RgdGC0LDQstC40YLRjC4=?=\n";
+print MAIL "To: $to\nFrom: noreply\@edomain.ru\nSubject: =?utf-8?B?0JLQsNGI0LUg0L/QuNGB0YzQvNC+INC90LUg0YPQtNCw0LvQvtGB0Ywg0LQ=?= =?utf-8?B?0L7RgdGC0LDQstC40YLRjC4=?=\n";
 print MAIL 'MIME-Version: 1.0', "\n";
 print MAIL 'Content-Type: text/html; charset="utf-8"', "\n";
 print MAIL 'Content-Transfer-Encoding: base64', "\n\n";
