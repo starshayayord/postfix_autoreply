cbd3bf62fd1699dce6192a019159f7d8787b5243
root <root@vm-spam-dmz.kontur>
2016-02-29 16:56:16 +0500

    code

---
+++ autoreply.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl
+
+use strict;
+use MIME::Base64;
+
+my ($to, $from) = @ARGV;
+$from =~ s/autoreply.e-kontur.ru/e-kontur.ru/;
+open MAIL, "| /usr/sbin/sendmail -t -oi";
+print MAIL "To: $to\nFrom: noreply\@e-kontur.ru\nSubject: =?utf-8?B?0JLQsNGI0LUg0L/QuNGB0YzQvNC+INC90LUg0YPQtNCw0LvQvtGB0Ywg0LQ=?= =?utf-8?B?0L7RgdGC0LDQstC40YLRjC4=?=\n";
+print MAIL 'MIME-Version: 1.0', "\n";
+print MAIL 'Content-Type: text/html; charset="utf-8"', "\n";
+print MAIL 'Content-Transfer-Encoding: base64', "\n\n";
+# get text from file
+open MSG, "/var/spool/filter/body.html";
+;my $msg = join ( "", <MSG> );
+print MAIL encode_base64($msg), "\n";
+close MSG;
+
+close MAIL;
